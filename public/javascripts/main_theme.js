cc0b3648ef17fe0d8d76964edabe66f64acd05f8
Vladimir Melnikovich <vmelnikovich@beltechsoftware.com>
2015-08-01 19:13:04 +0300

    Enhancement Commit

--- public/javascripts/main_theme.js
+++ public/javascripts/main_theme.js
@@ -1,5 +1,21 @@
 (function($){
    $(function() {
-       $('#check-code').click( function() { alert('Hello');});
+       $('#check-code').click( function() {
+         var $warning = $('.warning');
+         $warning.find('p').remove();
+         if (parseInt($('#code-number').val()) == 1 + 1 + 8 + 1 + 9 + 8 + 9) {
+             $('#step-1').hide();
+             $('#step-2').show();
+         } else  {
+             $warning.append('<p>Неверный Код! Попробуй Еще Раз!</p>')
+         }
+       });
+
+       $('#begin').click(function() {
+           $('#step-2').hide();
+           $('#step-3').show();
+           $('body').css('display', 'none');
+
+       });
    });
 })(jQuery);
