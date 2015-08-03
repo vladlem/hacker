3186fa051cf1676efc91a83b72a7a01b6942e7f0
Vladimir Melnikovich <vmelnikovich@beltechsoftware.com>
2015-08-03 14:44:35 +0300

    Added steps.

--- public/javascripts/main_theme.js
+++ public/javascripts/main_theme.js
@@ -6,6 +6,10 @@
          if (parseInt($('#code-number').val()) == 1 + 1 + 8 + 1 + 9 + 8 + 9) {
              $('#step-1').hide();
              $('#step-2').show();
+
+             setTimeout(function(){
+                 $('.first-task').slideDown(1000);
+             }, 1000);
          } else  {
              $warning.append('<p>Неверный Код! Попробуй Еще Раз!</p>')
          }
@@ -14,8 +18,48 @@
        $('#begin').click(function() {
            $('#step-2').hide();
            $('#step-3').show();
-           $('body').css('display', 'none');
+           $('body').css('display', 'none').css('background-color','midnightblue');
+       });
 
+       $('#second-task').click(function() {
+           $('#step-3').hide();
+           $('#step-4').show();
+       });
+
+       $('#check-name').click( function() {
+           var $warning = $('.warning');
+           $warning.find('p').remove();
+           if ($('#code-name').val() == '39lb650v') {
+               $('#step-4').hide();
+               $('#step-5').show();
+
+               setTimeout(function(){
+                   $('.first-task').slideDown(1000);
+               }, 1000);
+           } else  {
+               $warning.append('<p>Неверное Имя! Попробуй Еще Раз!</p>')
+           }
+       });
+
+       $('#third-task').click(function() {
+           $('#step-5').hide();
+           $('#step-6').show();
+           $('body').css('background-color','steelblue');
+       });
+
+       $('#varenik-code').click( function() {
+           var $warning = $('.warning');
+           $warning.find('p').remove();
+           if (parseInt($('#varenik-number').val()) == 3215) {
+               $('#step-6').hide();
+               $('#step-7').show();
+
+               setTimeout(function(){
+                   $('.first-task').slideDown(1000);
+               }, 1000);
+           } else  {
+               $warning.append('<p>Неверное Имя! Попробуй Еще Раз!</p>')
+           }
        });
    });
 })(jQuery);
